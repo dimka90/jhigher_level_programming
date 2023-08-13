2ebd5ce35bf4991ddf3b2ada44d2f5ca2e906d1b
Josephdada01 <ridwandada500@gmail.com>
2023-08-13 14:20:55 -0700

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    f_string = "{}\n" * len(my_list)
-    fm_string = f_string.format(*my_list)
-    print(fm_string, end="")
+    for m in my_list:
+        print("{}".format(m))
