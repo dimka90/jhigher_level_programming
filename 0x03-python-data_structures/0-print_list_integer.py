7a05855a5fd3287806f3651197e2c1fc09efbbc0
Josephdada01 <ridwandada500@gmail.com>
2023-08-13 13:52:44 -0700

    fixed format issues

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for m in my_list:
-        print("{}".format(m))
+    f_string = "{}\n" * len(my_list)
+    fm_string = f_string.format(*my_list)
+    print(fm_string, end="")
