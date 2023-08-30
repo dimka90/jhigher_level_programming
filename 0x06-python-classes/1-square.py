2d6c12e672d8a5690e40270a54b1f33e713d7355
Josephdada01 <ridwandada500@gmail.com>
2023-08-29 19:04:40 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""
+a class that defines a square
+"""
+
+
+class Square:
+    """
+    a class that defines a square
+    """
+    def __init__(self, size):
+        self.__size = size
